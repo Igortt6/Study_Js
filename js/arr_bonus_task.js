b6e1ffaecbf1b1e4ec845d8a28f92f81bc88dbd7
Ihor <Igor.tt6@gmail.com>
2022-06-21 15:38:05 +0300

    2.34 task 2.44 practic

--- js/arr_bonus_task.js
+++ js/arr_bonus_task.js
@@ -23,7 +23,7 @@ const shoppingMallData = {
     height: 5,
     moneyPer1m3: 30,
     budget: 50000
-}
+};
 
 
 function isBudgetEnough(data) {
@@ -37,10 +37,89 @@ function isBudgetEnough(data) {
     volume = data.height * square;
 
     if (data.budget - (volume * data.moneyPer1m3) > 0) {
-        console.log("хватает")
+        return "Бюджета достаточно";
     } else {
-        console.log("нехватает")
+        return "Бюджета недостаточно";
     }
 };
 
 isBudgetEnough(shoppingMallData);
+
+//  Список студентов для сортировки в нутри массива.
+
+const students = ['Peter', 'Andrew', 'Ann', 'Mark', 'Josh', 'Sandra', 'Cris', 'Bernard', 'Takesi', 'Sam'];
+
+function sortStudentsByGroups(arr) {
+    arr.sort();
+    const a = [], b = [], c = [], rest = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (i < 3) {
+            a.push(arr[i]);
+        } else if (i < 6) {
+            b.push(arr[i]);
+        } else if (i < 9) {
+            c.push(arr[i]);
+        } else {
+            rest.push(arr[i]);
+        }
+    }
+    return [a,b,c, `Оставшиеся студенты: ${rest.length === 0 ? '-' : rest.join(', ')}`]
+}
+
+sortStudentsByGroups(students);
+
+
+// Упрежнеине по написанию кода 15 2-37 
+// Ощбики в функция другого разработчика
+
+const restorantData = {
+    menu: [
+        {
+            name: 'Salad Caesar',
+            price: '14$'
+        },
+        {
+            name: 'Pizza Diavola',
+            price: '9$'
+        },
+        {
+            name: 'Beefsteak',
+            price: '17$'
+        },
+        {
+            name: 'Napoleon',
+            price: '7$'
+        }
+    ],
+    waitors: [
+        {name: 'Alice', age: 22}, {name: 'John', age: 24}
+    ],
+    averageLunchPrice: '20$',
+    openNow: true
+};
+z
+function isOpen(prop) {
+    let answer = '';
+    prop ? answer = 'Открыто' : answer = 'Закрыто';
+
+    return answer;
+}
+console.log(isOpen(restorantData.openNow));
+
+function isAverageLunchPriceTrue(fDish, sDish, average) {
+    if (+fDish.price.slice(0, -1) + (+sDish.price.slice(0, -1)) < +average.slice(0, -1)) {
+        return 'Цена ниже средней';
+    } else {
+        return 'Цена выше средней';
+    }
+}
+console.log(isAverageLunchPriceTrue(restorantData.menu[0], restorantData.menu[1], restorantData.averageLunchPrice));
+
+function transferWaitors(data) {
+    const copy = Object.assign({}, data);
+    copy.waitors = [{name: 'Mike', age: 32}];
+    return copy;
+}
+
+transferWaitors(restorantData); 
