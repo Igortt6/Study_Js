352562ebe0425bd1980f7ef90aa7cdabbb28b634
Ihor <Igor.tt6@gmail.com>
2022-07-08 16:24:41 +0300

    4.70 Timer Done

--- projects/Food/dist/js/script.js
+++ projects/Food/dist/js/script.js
@@ -94,9 +94,10 @@
 /***/ (function(module, exports) {
 
 window.addEventListener('DOMContentLoaded', () => {
+  // TABS
   const tabs = document.querySelectorAll('.tabheader__item'),
         tabsContent = document.querySelectorAll('.tabcontent'),
-        tabsParants = document.querySelector('.tabheader__items');
+        tabsParants = document.querySelector('.tabheader__items'); // Скрываем все табы 
 
   function hideTabContant() {
     tabsContent.forEach(item => {
@@ -109,7 +110,7 @@ window.addEventListener('DOMContentLoaded', () => {
     });
   }
 
-  ;
+  ; // показываем таб по индексу + значение по умолчанию
 
   function showTabContant(i = 0) {
     tabsContent[i].classList.add('show', 'fade');
@@ -119,7 +120,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
   ;
   hideTabContant();
-  showTabContant();
+  showTabContant(); // слушаем клин по таб меню и вызываем обе функции
+
   tabsParants.addEventListener('click', event => {
     const target = event.target;
 
@@ -131,7 +133,73 @@ window.addEventListener('DOMContentLoaded', () => {
         }
       });
     }
-  });
+  }); // TIMER
+  // 1 установить дату 
+  // 2 опеределить разницу между дедлайном
+  // 3 обновление таймера каждую минуту 
+
+  const deadline = '2022-07-10'; // установка конечной даты
+  // расчет даты в мс
+
+  function getTimeRemaining(endtime) {
+    let days, hours, minutes, seconds;
+    const t = Date.parse(endtime) - Date.parse(new Date()); // разница в датах в Мс
+    // проверка на отрицательные значения.
+
+    if (t <= 0) {
+      days = 0;
+      hours = 0;
+      minutes = 0;
+      seconds = 0;
+    } else {
+      days = Math.floor(t / (1000 * 60 * 60 * 24)), // разница в часах + округление
+      hours = Math.floor(t / (1000 * 60 * 60) % 24), minutes = Math.floor(t / (1000 * 60) % 60), seconds = Math.floor(t / 1000 % 60);
+    } // возвращение расчитаных значений в виде объекта
+
+
+    return {
+      'total': t,
+      'days': days,
+      'hours': hours,
+      'minutes': minutes,
+      'seconds': seconds
+    };
+  }
+
+  function gerZero(num) {
+    if (num >= 0 && num < 10) {
+      return `0${num}`;
+    } else {
+      return num;
+    }
+  } // функция 
+
+
+  function setClock(selector, endtime) {
+    const timer = document.querySelector(selector),
+          days = timer.querySelector('#days'),
+          hours = timer.querySelector('#hours'),
+          minutes = timer.querySelector('#minutes'),
+          seconds = timer.querySelector('#seconds'),
+          timeInterval = setInterval(updateClock, 1000); // установка автообновления таймера
+
+    updateClock(); // вызов функции для избежания задержки в 1000 мс в верстке
+
+    function updateClock() {
+      const t = getTimeRemaining(endtime); // записываем значения на страницу
+
+      days.innerHTML = gerZero(t.days);
+      hours.innerHTML = gerZero(t.hours);
+      minutes.innerHTML = gerZero(t.minutes);
+      seconds.innerHTML = gerZero(t.seconds);
+
+      if (t.total <= 0) {
+        clearInterval(timeInterval);
+      }
+    }
+  }
+
+  setClock('.timer', deadline);
 });
 
 /***/ })
