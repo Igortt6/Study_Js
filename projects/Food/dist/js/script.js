e3ebbf523e329b92b59276ddd9ce1500d5dde00c
Ihor <Igor.tt6@gmail.com>
2022-07-11 13:25:56 +0300

    4.79 cards from dinamic JS Done

--- projects/Food/dist/js/script.js
+++ projects/Food/dist/js/script.js
@@ -240,8 +240,7 @@ window.addEventListener('DOMContentLoaded', () => {
     if (e.code === "Escape" && modal.classList.contains('show')) {
       closeModal();
     }
-  });
-  const modalTimerId = setTimeout(openModal, 5000); // открывать модал через N секунд
+  }); // const modalTimerId = setTimeout(openModal, 5000); // открывать модал через N секунд
 
   function showModalByScroll() {
     if (window.pageYOffset + document.documentElement.clientHeight >= document.documentElement.scrollHeight - 1) {
@@ -251,6 +250,47 @@ window.addEventListener('DOMContentLoaded', () => {
   }
 
   window.addEventListener('scroll', showModalByScroll); // вызываем модалку при прокрутве в низ стриници
+  // Используем классы для карточек
+
+  class MenuCard {
+    constructor(src, alt, title, descr, price, parentSelector) {
+      this.src = src;
+      this.alt = alt;
+      this.title = title;
+      this.descr = descr;
+      this.price = price;
+      this.parent = document.querySelector(parentSelector); // куда будем пушить объект
+
+      this.transfer = 35; // курст вальют 
+
+      this.changeToUAH();
+    }
+
+    changeToUAH() {
+      this.price = this.price * this.transfer;
+    }
+
+    render() {
+      const element = document.createElement('div');
+      element.innerHTML = `
+                <div class="menu__item">
+                    <img src=${this.src} alt=${this.alt}>
+                    <h3 class="menu__item-subtitle">${this.title}</h3>
+                    <div class="menu__item-descr">${this.descr}</div>
+                    <div class="menu__item-divider"></div>
+                    <div class="menu__item-price">
+                        <div class="menu__item-cost">Цена:</div>
+                        <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                    </div>
+                </div>
+            `;
+      this.parent.append(element); // метод пуша
+    }
+
+  } // данные которые будет пушить
+
+
+  new MenuCard('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!', 9, '.menu .container').render();
 });
 
 /***/ })
