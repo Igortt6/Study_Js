a4e2caf06118237e3eb62455195d7abf61352f91
Ihor <Igor.tt6@gmail.com>
2022-07-28 12:42:04 +0300

    Slider regular expretion 5.96

--- projects/Food/src/js/main.js
+++ projects/Food/src/js/main.js
@@ -522,14 +522,19 @@ window.addEventListener('DOMContentLoaded', () =>{
         dots[slideIndex -1].style.opacity = 1;
     };
 
+    // превращаем 500px в 500.
+    const deleteNotDigits = function (str) {
+        return +str.replace(/\D/g, '') // удаляем все НЕ цифры 
+    };
+
 
     // offset = ширина 1 слайда * количество слайдов-1
     //Обработчик событий КЛИК - навигация на следующий слайд
     nextSlide.addEventListener('click', () => {
-        if (offset == +width.slice(0, width.length - 2) * (slides.length - 1)){ // 650 * 4-1
+        if (offset == deleteNotDigits(width) * (slides.length - 1)){ // 650 * 4-1
             offset = 0;
         } else {
-            offset += +width.slice(0, width.length - 2);
+            offset += deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -548,9 +553,9 @@ window.addEventListener('DOMContentLoaded', () =>{
     //Обработчик событий КЛИК - навигация на предидущий слайд
     prevSlide.addEventListener('click', () => {
         if (offset == 0){
-            offset = +width.slice(0, width.length - 2) * (slides.length - 1)
+            offset = deleteNotDigits(width) * (slides.length - 1)
         } else {
-            offset -= +width.slice(0, width.length - 2);
+            offset -= deleteNotDigits(width);
         }
 
         slidesField.style.transform = `translateX(-${offset}px)`;
@@ -573,7 +578,7 @@ window.addEventListener('DOMContentLoaded', () =>{
             const slideTo = e.target.getAttribute('data-slide-to')
 
             slideIndex = slideTo; // Привязываем точку к  активному слайду
-            offset = +width.slice(0, width.length - 2) * (slideTo - 1)
+            offset = deleteNotDigits(width) * (slideTo - 1)
 
             slidesField.style.transform = `translateX(-${offset}px)`;
 
@@ -586,5 +591,9 @@ window.addEventListener('DOMContentLoaded', () =>{
     // получаем весь слайдер
     // устанавливаем position relative для позиционрирования точек 
     // перебором устанавливаем количество точек = количеству слайдеров
+
+// _____________________________________________________________________________________________________________________________
+// Калькулятор
+
 
 });
